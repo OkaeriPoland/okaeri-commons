f7367884a9eb78ede485f4de3cab15ed807da34d
PinkLolicorn <pinklolicorngithub@daffit.pl>
2021-10-02 04:38:24 +0200

    Add AllWorldsRunnable#consuming, OnlinePlayersRunnable#consuming, release 0.1.7

--- bukkit/src/main/java/eu/okaeri/commons/bukkit/task/AllWorldsRunnable.java
+++ bukkit/src/main/java/eu/okaeri/commons/bukkit/task/AllWorldsRunnable.java
@@ -3,7 +3,18 @@ package eu.okaeri.commons.bukkit.task;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 
+import java.util.function.Consumer;
+
 public abstract class AllWorldsRunnable implements Runnable {
+
+    public static Runnable consuming(Consumer<World> consumer) {
+        return new AllWorldsRunnable() {
+            @Override
+            public void runFor(World world) {
+                consumer.accept(world);
+            }
+        };
+    }
 
     @Override
     public void run() {
--- bukkit/src/main/java/eu/okaeri/commons/bukkit/task/OnlinePlayersRunnable.java
+++ bukkit/src/main/java/eu/okaeri/commons/bukkit/task/OnlinePlayersRunnable.java
@@ -3,7 +3,18 @@ package eu.okaeri.commons.bukkit.task;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
+import java.util.function.Consumer;
+
 public abstract class OnlinePlayersRunnable implements Runnable {
+
+    public static Runnable consuming(Consumer<Player> consumer) {
+        return new OnlinePlayersRunnable() {
+            @Override
+            public void runFor(Player player) {
+                consumer.accept(player);
+            }
+        };
+    }
 
     @Override
     public void run() {
