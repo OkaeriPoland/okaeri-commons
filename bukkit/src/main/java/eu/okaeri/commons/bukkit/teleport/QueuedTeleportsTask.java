6408fe94dcf9b3d9616ea96ba07784ae2a8e6754
PinkLolicorn <pinklolicorngithub@daffit.pl>
2022-04-20 09:57:59 +0200

    Hide QueuedTeleports messages by default, release 0.2.13

--- bukkit/src/main/java/eu/okaeri/commons/bukkit/teleport/QueuedTeleportsTask.java
+++ bukkit/src/main/java/eu/okaeri/commons/bukkit/teleport/QueuedTeleportsTask.java
@@ -7,6 +7,8 @@ import java.util.Queue;
 
 public class QueuedTeleportsTask implements Runnable {
 
+    private static final boolean DEBUG = Boolean.parseBoolean(System.getProperty("okaeri.platform.debug", "false"));
+
     private final Queue<TeleportAction> queue;
     private final Plugin plugin;
     private final int teleportsPerRun;
@@ -36,11 +38,13 @@ public class QueuedTeleportsTask implements Runnable {
             actionsPerformed++;
         }
 
-        if (actionsPerformed == 0) {
+        if ((actionsPerformed == 0) || !DEBUG) {
             return;
         }
 
         long took = System.nanoTime() - start;
-        this.plugin.getLogger().info("Teleport Queue [-" + actionsPerformed + "]: " + this.queue.size() + " actions left (" + took + " ns, " + took / 1000L / 1000L + " ms)");
+        long tookMs = took / 1000L / 1000L;
+
+        this.plugin.getLogger().info("Teleport Queue [-" + actionsPerformed + "]: " + this.queue.size() + " actions left (" + took + " ns, " + tookMs + " ms)");
     }
 }
