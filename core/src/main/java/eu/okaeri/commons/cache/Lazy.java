cfa0d6b7173221af7af23d12a5013bd804f27eba
Sandra <sandra@okaeri.cloud>
2022-11-13 09:32:18 +0100

    Release 0.2.21

--- core/src/main/java/eu/okaeri/commons/cache/Lazy.java
+++ core/src/main/java/eu/okaeri/commons/cache/Lazy.java
@@ -11,9 +11,9 @@ import java.util.function.Supplier;
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public class Lazy<T> implements Supplier<T>, Runnable {
 
-    @NonNull protected Supplier<T> supplier;
-    @Getter protected Instant lastUpdated;
-    @Getter protected T value;
+    protected @NonNull Supplier<T> supplier;
+    protected @Getter Instant lastUpdated;
+    protected @Getter T value;
 
     public static <A> Lazy<A> of(@NonNull Supplier<A> supplier) {
         return new Lazy<>(supplier);
